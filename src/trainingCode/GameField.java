297f846867d4856f24d922873c9005156f3d51c3
Ildar <example.ru>
2022-07-28 00:35:56 +0500

    game finish, i do not understand

--- src/trainingCode/GameField.java
+++ src/trainingCode/GameField.java
@@ -1,9 +1,12 @@
 package trainingCode;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class GameField {
     public static int SCREEN_HEIGHT = 20;
+
     public static int SCREEN_WIDTH = 20;
 
     Cell[][] gameScreen = new Cell[SCREEN_HEIGHT][SCREEN_WIDTH];
@@ -12,15 +15,61 @@ public class GameField {
         for (int i = 0; i < SCREEN_HEIGHT; i++) {
 
             for (int j = 0; j < SCREEN_WIDTH; j++) {
-                Random random = new Random();
-                boolean isCellAlive = random.nextBoolean();
-                gameScreen[i][j] = new Cell(i, j, isCellAlive);
+              //  Random random = new Random();
+              //  boolean isCellAlive = random.nextBoolean();
+                gameScreen[i][j] = new Cell(i, j, false);
             }
         }
     }
 
     void simulateNextStep() {
+        for (int i = 0; i < SCREEN_HEIGHT; i++) {
+            for (int j = 0; j < SCREEN_WIDTH; j++) {
+                Cell current = gameScreen[i][j];
+                long aliveNeigb = calculateNeibours(current, gameScreen);
 
+                if (!current.alive && aliveNeigb == 3) {
+                    current.alive = true;
+                } else if (current.alive && (aliveNeigb == 2 || aliveNeigb == 3)) {
+                    current.alive = true;
+                } else {
+                    current.alive = false;
+                }
+            }
+        }
+    }
+
+    private long calculateNeibours(Cell cell, Cell[][] gameScreen) {
+        int x = cell.x;
+        int y = cell.y;
+        var a = getCellByCoords(x - 1, y - 1, gameScreen);
+        var b = getCellByCoords(x, y - 1, gameScreen);
+        var c = getCellByCoords(x + 1, y - 1, gameScreen);
+
+        var d = getCellByCoords(x - 1, y, gameScreen);
+        var e = getCellByCoords(x + 1, y, gameScreen);
+
+        var f = getCellByCoords(x - 1, y + 1, gameScreen);
+        var g = getCellByCoords(x, y + 1, gameScreen);
+        var h = getCellByCoords(x + 1, y + 1, gameScreen);
+
+        return Stream.of(a, b, c, d, e, f, g, h)
+                .filter(cc -> cc != null)
+                .filter(cc -> cc.alive)
+                .count();
+    }
+
+
+    private boolean isALiveCell(int x, int y, Cell[][] gameScreen) {
+        if (x < 0 || y < 0) return false;
+        if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT) return false;
+        return gameScreen[x][y].alive;
+    }
+
+    private Cell getCellByCoords(int x, int y, Cell[][] gameScreen) {
+        if (x < 0 || y < 0) return null;
+        if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT) return null;
+        return gameScreen[x][y];
     }
 
     void print() {
@@ -40,3 +89,5 @@ public class GameField {
         System.out.println("_________________________________________________________");
     }
 }
+
+
--- src/trainingCode/Main.java
+++ src/trainingCode/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         GameField g = new GameField();
+        g.gameScreen[0][0].alive = true;
+        g.gameScreen[0][1].alive = true;
+        g.gameScreen[1][0].alive = true;
+        g.gameScreen[1][1].alive = true;
+        g.gameScreen[12][11].alive = true;
+        g.gameScreen[11][11].alive = true;
+        g.gameScreen[1][1].alive = true;
+
+        g.gameScreen[13][13].alive = true;
+        g.gameScreen[14][14].alive = true;
+        g.gameScreen[14][15].alive = true;
+        g.gameScreen[15][14].alive = true;
+        g.gameScreen[15][15].alive = true;
+
+//        g.gameScreen[1][1].alive = true;
+//        g.gameScreen[1][1].alive = true;
 
         while (true) {
             g.simulateNextStep();
